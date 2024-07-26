f42cc2f505b004e90c5f59ac9a529286e0e9ca8f
Murphie Pettyjohn <98448438+Mothpy@users.noreply.github.com>
2024-07-26 16:25:43 -0700

    initial upload

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const pastes = require("./data/pastes-data");
+const pastesRouter = require("./pastes/pastes.router");
+
+app.use(express.json());
+
+app.use("/pastes", pastesRouter); // Note: app.use
+
+
+// Not found handler
+app.use((request, response, next) => {
+  next(`Not found: ${request.originalUrl}`);
+});
+
+// Error handler
+app.use((error, request, response, next) => {
+  console.error(error);
+  const { status = 500, message = "Something went wrong!" } = error;
+  response.status(status).json({ error: message });
+});
+
+module.exports = app;
